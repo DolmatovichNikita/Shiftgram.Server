dbd9cea557bb3400d6c8a51ba791a22122e3ac20
dolmatovich.nikita <dolmatovich.nikita96@gmail.com>
2018-08-28 20:21:58 +0300

    Define IAccountRepository commit

---
+++ Shiftgram/Shiftgram.Core/Repository/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Shiftgram.Core.Models;
+using Shiftgram.Core.Strategy;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shiftgram.Core.Repository
+{
+	public interface IAccountRepository: IRepository<Account>, IUpdatableAccount
+	{
+		Task<IEnumerable<Account>> GetAll();
+		Task<Account> GetByPhone(string phone);
+	}
+}
---
+++ Shiftgram/Shiftgram.Core/Strategy/IUpdatable.cs
@@ -0,0 +1,10 @@
+﻿using Shiftgram.Core.Enums;
+using System.Threading.Tasks;
+
+namespace Shiftgram.Core.Strategy
+{
+	public interface IUpdatableAccount
+	{
+		Task<DbAnswerCode> Update();
+	}
+}
