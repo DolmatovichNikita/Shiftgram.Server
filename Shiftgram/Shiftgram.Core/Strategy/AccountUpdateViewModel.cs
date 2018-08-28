b93496002f2582402b1cd981933a7e38c1402f52
dolmatovich.nikita <dolmatovich.nikita96@gmail.com>
2018-08-28 20:41:03 +0300

    Add view models for update account commit

---
+++ Shiftgram/Shiftgram.Core/Strategy/AccountUpdateViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Shiftgram.Core.Strategy
+{
+	public class AccountUpdateViewModel
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Bio { get; set; }
+		public string Username { get; set; }
+		public string PhotoUrl { get; set; }
+		public string PhoneCode { get; set; }
+		public string Phone { get; set; }
+		public string GenderName { get; set; }
+		public string UpdateType { get; set; }
+	}
+}
