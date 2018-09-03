72cdc5587f3730b1090147cae29718fc3694d5f5
dolmatovich.nikita <dolmatovich.nikita96@gmail.com>
2018-09-03 20:46:50 +0300

    Define IVerificationRepository interface commit

---
+++ Shiftgram/Shiftgram.Core/Repository/IVerificationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Shiftgram.Core.Models;
+using System.Threading.Tasks;
+
+namespace Shiftgram.Core.Repository
+{
+	public interface IVerificationRepository: IRepository<Verification>
+	{
+		Task Update(string phone);
+	}
+}
