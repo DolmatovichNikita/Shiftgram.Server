e8d109f54eef89a73bdc151121ae4592afe0970f
dolmatovich.nikita <dolmatovich.nikita96@gmail.com>
2018-09-05 20:05:10 +0300

    Define iPhoneRepository commit

---
+++ Shiftgram/Shiftgram.Core/Repository/IPhoneRepository.cs
@@ -0,0 +1,11 @@
+﻿using Shiftgram.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shiftgram.Core.Repository
+{
+	public interface IPhoneRepository
+	{
+		Task<IEnumerable<Phone>> GetPhones();
+	}
+}
