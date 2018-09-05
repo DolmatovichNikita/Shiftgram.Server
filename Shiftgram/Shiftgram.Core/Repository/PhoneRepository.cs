93edfea2d67bd2b13cb10bcce3b42e309eca3a62
dolmatovich.nikita <dolmatovich.nikita96@gmail.com>
2018-09-05 20:07:13 +0300

    Implement PhoneRepository commit

---
+++ Shiftgram/Shiftgram.Core/Repository/PhoneRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Shiftgram.Core.Models;
+
+namespace Shiftgram.Core.Repository
+{
+	public class PhoneRepository : IPhoneRepository
+	{
+		private ShiftgramContext _context;
+
+		public PhoneRepository()
+		{
+			this._context = new ShiftgramContext();
+		}
+
+		public async Task<IEnumerable<Phone>> GetPhones()
+		{
+			return await this._context.Phones.ToListAsync();
+		}
+	}
+}
