97cab0db00ded0f3453e96fef79843023469a800
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-09-24 11:41:47 +0300

    Add test method for adding account commit

---
+++ Shiftgram/Shiftgram.Tests/AccountTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using Shiftgram.AccountServer.Controllers;
+using Shiftgram.Core.Models;
+using Shiftgram.Core.Repository;
+using System.Linq;
+using System;
+
+namespace Shiftgram.Tests
+{
+    [TestFixture]
+    public class AccountTest
+    {
+        [Test]
+        public async Task TestIsAddAccount()
+        {
+            Mock<IAccountRepository> mock = new Mock<IAccountRepository>();
+            mock.Setup(x => x.GetAll()).ReturnsAsync(new List<Account>
+            {
+                new Account {Id = 1}
+            });
+            AccountController controller = new AccountController(mock.Object);
+
+            Account account = new Account() { FirstName = "TestName" };
+            await controller.AddAccount(account);
+
+            mock.Verify(x => x.Add(account), Times.Once);
+        }
+    }
+}
