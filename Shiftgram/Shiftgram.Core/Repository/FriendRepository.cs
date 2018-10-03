b3369869458d3c7502e292f37d50d4da04b5e30c
dolmatovich.nikita <dolmatovich.nikita96@gmail.com>
2018-10-03 20:09:08 +0300

    Update realization of method for adding friend commit

--- Shiftgram/Shiftgram.Core/Repository/FriendRepository.cs
+++ Shiftgram/Shiftgram.Core/Repository/FriendRepository.cs
@@ -22,14 +22,18 @@ namespace Shiftgram.Core.Repository
 
 		public async Task<int> Add(Friend item)
 		{
-			this._context.Friends.Add(item);
-			int rows = await this._context.SaveChangesAsync();
-
-			if(rows > 0)
+			var model = await this._context.Friends.FirstOrDefaultAsync<Friend>(x => x.AccountAId == item.AccountAId && x.AccountBId == item.AccountBId);
+			if(model == null)
 			{
-				return item.Id;
-			}
+				this._context.Friends.Add(item);
+				int rows = await this._context.SaveChangesAsync();
 
+				if (rows > 0)
+				{
+					return item.Id;
+				}
+			}
+			
 			throw new AccountException();
 		}
 
