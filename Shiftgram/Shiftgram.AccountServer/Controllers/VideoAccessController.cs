b7aa8e598138d3e074554d13deceb2b0a2360e61
dolmatovich.nikita <dolmatovich.nikita96@gmail.com>
2018-10-10 17:17:45 +0300

    Add resources to AccountServer project commit

--- Shiftgram/Shiftgram.AccountServer/Controllers/VideoAccessController.cs
+++ Shiftgram/Shiftgram.AccountServer/Controllers/VideoAccessController.cs
@@ -1,23 +1,28 @@
 ﻿using Shiftgram.AccountServer.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using Twilio.Jwt.AccessToken;
+using System.Resources;
+using System.Reflection;
 
 namespace Shiftgram.AccountServer.Controllers
 {
 	[RoutePrefix("api/videoaccess")]
     public class VideoAccessController : ApiController
     {
+		private ResourceManager _manager;
+
+		public VideoAccessController()
+		{
+			this._manager = new ResourceManager("Shiftgram.AccountServer.Resources", Assembly.GetExecutingAssembly());
+		}
+
 		[HttpPost]
 		public IHttpActionResult GetAccessToken(AccessTokenModel model)
 		{
-			string accountSid = "ACf2a5fcdbf1a63fc1e5f21610b4cd68a0";
-			string apiKeySid = "SKbee5c87f96823b291c1a1b745e379c2f";
-			string apiKeySecret = "SKbee5c87f96823b291c1a1b745e379c2f";
+			string accountSid = this._manager.GetString("AccountSID");
+			string apiKeySid = this._manager.GetString("AccountApiKeySID");
+			string apiKeySecret = this._manager.GetString("ApiKeySecret");
 			string identity = model.Name;
 			string room = model.Room;
 
